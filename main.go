4c70ce3a78c13bba9aa7c56f21b8aea1bc8c01f8
Elson Rodriguez <elson.rodriguez@gmail.com>
2017-05-24 21:47:12 -0700

    Identifying services with/without ingress ips

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "log"
     "io/ioutil"
     "os"
+    "os/user"
     "net/http"
 
     "github.com/ghodss/yaml"
@@ -31,33 +32,57 @@ func loadClient(kubeconfigPath string) (*k8s.Client, error) {
 func main() {
     kubernetes_host := os.Getenv("KUBERNETES_SERVICE_HOST")
     kubeconfig := os.Getenv("KUBECONFIG")
+    in_cluster := os.Getenv("MINIKUBELB_IN_CLUSTER")
 
-    var client k8s.Client
-    var err error
+    usr, err := user.Current()
+    if err != nil {
+        log.Fatal( err )
+    }
 
-    if kubernetes_host == "" {
+
+    if kubeconfig == "" {
+        kubeconfig = usr.HomeDir + "/.kube/config"
+    }
+
+    if _, err := os.Stat(kubeconfig); os.IsNotExist(err) {
+        fmt.Printf("kubeconfig not found at %v\n", kubeconfig)
+        kubeconfig = "" 
+    }
+
+
+    var client *k8s.Client
+
+    if kubernetes_host != "" || in_cluster == "True" {
         client, err = k8s.NewInClusterClient()
         if err != nil {
             log.Fatal(err)
         }
+
     } else if kubeconfig != "" {
         client, err = loadClient(kubeconfig)
         if err != nil {
             log.Fatal(err)
         }
-    } else if kubeconfig == "" {
-	client = k8s.Client{
-                    Endpoint:  "http://127.0.0.1:8080",
+    } else {
+	client = &k8s.Client{
+                    Endpoint:  "http://127.0.0.1:8001",
                     Namespace: "default",
                     Client: &http.Client{} }
     }
-//    client := k8s.Client{}
 
-    nodes, err := client.CoreV1().ListNodes(context.Background())
+    svc, err := client.CoreV1().ListServices(context.Background(), "")
     if err != nil {
         log.Fatal(err)
     }
-    for _, node := range nodes.Items {
-        fmt.Printf("name=%q schedulable=%t\n", *node.Metadata.Name, !*node.Spec.Unschedulable)
+    for _, svc := range svc.Items {
+        //fmt.Println(reflect.TypeOf(svc.Status.LoadBalancer.Ingress))
+        if *svc.Spec.Type == "LoadBalancer" && len(svc.Status.LoadBalancer.Ingress) == 0 {
+          fmt.Printf("Service %q has no ingress for its loadbalancer\n", *svc.Metadata.Name)
+        } else if *svc.Spec.Type == "LoadBalancer" {
+           fmt.Printf("Service %q has IP %v for its loadbalancer\n", *svc.Metadata.Name, *svc.Status.LoadBalancer.Ingress[0].Ip) //need to check for hostnames and other types too.
+        } else {
+           fmt.Printf("Service %q is not type LoadBalancer\n", *svc.Metadata.Name)
+        }
     }
 }
+
